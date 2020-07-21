5da0735b32d7ef0fe0aa31f2c74d433ae5d2ee80
GreenCyberNinja <1824@holbertonschool.com>
2020-07-21 03:16:59 +0000

    string length tool

---
+++ _strln.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strln - string length
+* @s: input string
+* Return: size of string
+*/
+int _strln(char *s)
+{
+	int n;
+	for (n = 0; *s !='\0'; n++)
+	continue;
+
+	return (n);
+}
