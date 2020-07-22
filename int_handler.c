c53357fee8c2ec30bc8792d3eb8bb2ee91f615d9
GreenCyberNinja <1824@holbertonschool.com>
2020-07-22 04:16:07 +0000

    betty compliant

--- int_handler.c
+++ int_handler.c
@@ -1,9 +1,7 @@
 #include "holberton.h"
 /**
-*
-*
-*
-*
+* rev_string - reverses string
+* @s: string to reverse
 */
 void rev_string(char *s)
 {
@@ -19,16 +17,17 @@ void rev_string(char *s)
 	}
 }
 /**
-*
-*
-*
+* int_handler - handles int conversion
+* @vl: va_arg list
+* Return: void
 */
 int int_handler(va_list vl)
 {
 	int i = va_arg(vl, int);
+
 	while ((i /= 10) > 0)
 	{
 		print_c(i % 10 + '0');
 	}
-	return(0);
+	return (0);
 }
