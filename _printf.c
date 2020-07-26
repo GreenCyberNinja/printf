cdcf040ad1ef874d8627e142938f3ef180de07d8
GreenCyberNinja <1824@holbertonschool.com>
2020-07-26 22:01:17 +0000

    betty compliant and % edge case fixed

--- _printf.c
+++ _printf.c
@@ -10,17 +10,14 @@ int _printf(const char *format, ...)
 	va_list vl;
 	int i, j, sum = 0, match = 0;
 
-
 	func_t func[] = {
 		{'d', point_handler},
 		{'s', string_handler},
 		{'c', char_handler},
 		{'i', int_handler},
-		{'\0', NULL}
-	};
+		{'\0', NULL} };
 	if (format == NULL)
 		return (0);
-
 	va_start(vl, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
@@ -29,7 +26,6 @@ int _printf(const char *format, ...)
 		{
 			for (j = 0; func[j].s != '\0'; j++)
 			{
-
 				if (format[i + 1] == func[j].s && format[i + 1] != '\0')
 				{
 					sum += func[j].f(vl);
@@ -38,9 +34,12 @@ int _printf(const char *format, ...)
 				}
 			}
 			if (format[i + 1] == '%')
+			{
 				i += 1;
+				match += 1;
+			}
 		}
-		if (format[i] != '\0')	
+		if (format[i] != '\0')
 			print_c(format[i]);
 		else
 			break;
