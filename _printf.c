9b404204034b820744f35f719b38439b52354fd2
GreenCyberNinja <1824@holbertonschool.com>
2020-07-26 19:19:50 +0000

    string length fix

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 		{'i', int_handler},
 		{'\0', NULL}
 	};
+	if (format == NULL)
+		return (0);
 
 	va_start(vl, format);
 	for (i = 0; format[i] != '\0'; i++)
@@ -27,17 +29,18 @@ int _printf(const char *format, ...)
 			for (j = 0; func[j].s != '\0'; j++)
 			{
 
-				if (format[i + 1] == func[j].s && format[i + 1] != '\0')
+				if (format[i + 1] == func[j].s && format[i + 1] != '%')
 				{
 					func[j].f(vl);
-					i += 1;
+					i += 2;
 				}
 			}
-			i += 1;
+			if (format[i + 1] == '%')
+				i += 1;
 		}
-		if (format[i] != '\0')
+		if (format[i] != '\0')	
 			print_c(format[i]);
 	}
 	va_end(vl);
-	return(0);
+	return(i);
 }
